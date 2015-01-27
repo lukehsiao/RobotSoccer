a72cfb08e845407f2f5d66481eed0a463e6f99f5
Luke Hsiao <lukehsiao@byu.edu>
2015-01-27 12:57:19 -0700

    Added tracking

--- ComputerVision/src/ComputerVision.cpp
+++ ComputerVision/src/ComputerVision.cpp
@@ -8,15 +8,15 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace cv;
 using namespace std;
 
  int main( int argc, char** argv )
  {
-    VideoCapture cap(0); //capture the video from web cam
+    VideoCapture cap(0); //capture the video from webcam
 
     if ( !cap.isOpened() )  // if not success, exit program
     {
@@ -26,64 +26,96 @@ using namespace std;
 
     namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
-	int iLowH = 0;
-	int iHighH = 179;
+  int iLowH = 170;
+  int iHighH = 179;
 
-	int iLowS = 0;
-	int iHighS = 255;
+  int iLowS = 150; 
+  int iHighS = 255;
 
-	int iLowV = 0;
-	int iHighV = 255;
+  int iLowV = 60;
+  int iHighV = 255;
 
-	//Create trackbars in "Control" window
-	cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-	cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+  //Create trackbars in "Control" window
+  createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+  createTrackbar("HighH", "Control", &iHighH, 179);
 
-	cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-	cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+  createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+  createTrackbar("HighS", "Control", &iHighS, 255);
 
-	cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
-	cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+  createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+  createTrackbar("HighV", "Control", &iHighV, 255);
 
-    while (true)
-    {
-        Mat imgOriginal;
+  int iLastX = -1; 
+  int iLastY = -1;
 
-        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+  //Capture a temporary image from the camera
+  Mat imgTmp;
+  cap.read(imgTmp); 
 
-         if (!bSuccess) //if not success, break loop
-        {
-             cout << "Cannot read a frame from video stream" << endl;
-             break;
-        }
+  //Create a black image with the size as the camera output
+  Mat imgLines = Mat::zeros( imgTmp.size(), CV_8UC3 );;
 
-	Mat imgHSV;
+  while (true)
+  {
+    Mat imgOriginal;
 
-	cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+    bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-	Mat imgThresholded;
 
-	inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
 
-	//morphological opening (remove small objects from the foreground)
-	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-	dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-
-	//morphological closing (fill small holes in the foreground)
-	dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-
-	imshow("Thresholded Image", imgThresholded); //show the thresholded image
-	imshow("Original", imgOriginal); //show the original image
-
-    if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-       {
-            cout << "esc key is pressed by user" << endl;
-            break;
-       }
+    if (!bSuccess) {//if not success, break loop
+      cout << "Cannot read a frame from video stream" << endl;
+      break;
     }
 
-   return 0;
+    Mat imgHSV;
 
+    cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+    Mat imgThresholded;
+
+    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+          
+    //morphological opening (removes small objects from the foreground)
+    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+    //morphological closing (removes small holes from the foreground)
+    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+    //Calculate the moments of the thresholded image
+    Moments oMoments = moments(imgThresholded);
+
+    double dM01 = oMoments.m01;
+    double dM10 = oMoments.m10;
+    double dArea = oMoments.m00;
+
+    // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+    if (dArea > 10000) {
+      //calculate the position of the ball
+      int posX = dM10 / dArea;
+      int posY = dM01 / dArea;        
+              
+      if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0) {
+        //Draw a red line from the previous point to the current point
+        line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
+      }
+
+      iLastX = posX;
+      iLastY = posY;
+    }
+
+    imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
+    imgOriginal = imgOriginal + imgLines;
+    imshow("Original", imgOriginal); //show the original image
+
+    if (waitKey(30) == 27) { //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+      cout << "esc key is pressed by user" << endl;
+      break; 
+    }
+  }
+  return 0;
 }
-////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////　
